7a262bfd20818747919f9d605fe9deec913f2c00
Bradley BARBIER <bradley.barbier@outlook.fr>
2022-04-06 15:59:14 +0200

    Create query for the connexion in Login Page

--- routes/rule.php
+++ routes/rule.php
@@ -7,27 +7,64 @@ $match = $router -> match();
 
 // Fait correspondre la cible précisé avec la bonne vue.
 
-if( is_array($match)) {
+if(is_array($match)) {
+    // Règles de redirection -> Rôles
+    
+    if($match['target'] == "home" && $_SESSION['role'] == 'visitor'){
+        header('Location:' . HTML_ROOT . '/');
+        die();
+    }
+    
+    if($match['target'] == "login" && $_SESSION['role'] == 'admin'){
+        header('Location:' . HTML_ROOT . '/home');
+        die();
+    }
+    
+    if(!empty($_POST)){    
+        if($match['target'] == 'login'){
+            if (isset($_POST['email'], $_POST['password'])){
+                $req = $db->query('SELECT * FROM `user` WHERE `email` = :email', [
+                    ':email' => $_POST['email']
+                ]);
+                $result = $req->fetch();
+                if($result){
+                    if(password_verify($_POST['password'], $result['password'])){
+                        $_SESSION['role'] = 'admin';
+                        $_SESSION['name'] = $result['surname'];
+                        $_SESSION['id_user'] = $result['id'];
+                        header('Location: ' . HTML_ROOT . '/admin');
+                        die();
+                    }
+                } else {
+                    $error = 'Votre email ou votre mot de passe est incorrect.';
+                }
+            } else {
+                $error = 'Vous n\'avez pas rempli tous les champs.';
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    
+    
     require 'assets/components/header.php';
     // if($match['target'] == "home") {
-    //     require 'assets/components/sidebar.php';
-    // }
-    include "./views/{$match['target']}.view.php";
-} else {
-    require 'assets/components/header.php';
+        //     require 'assets/components/sidebar.php';
+        // }
+        include "./views/{$match['target']}.view.php";
+    } else {
+        require 'assets/components/header.php';
     include "./views/404.view.php";
 }
 
-
-// Règles de redirection -> Rôles
-
-// if($match['target'] == "home" && $_SESSION['role'] == 'visitor'){
-//     header('Location:' . HTML_ROOT . '/');
-// }
-
-// if($match['target'] == "login" && $_SESSION['role'] == 'student'){
-//     header('Location:' . HTML_ROOT . '/home');
-// }
 
 
 require 'assets/components/footer.php';
--- views/login.view.php
+++ views/login.view.php
@@ -1,20 +1,29 @@
 <title>Login</title>
 
+
+
+
 <div class="login">
-        <div class="content">
-            <h1 id="title">Connexion</h1>
-            <form class="form" action="" method="POST">
-                <div class="field">
-                    <label>Nom d'utilisateur</label>
-                    <input type="text" required>
-                </div>
-                <div class="field">
-                    <label>Mot de passe</label>
-                    <input type="password" required>
-                </div>
-                <div class="submit">
-                    <input type="submit" value="Se connecter">
-                </div>
-            </form>
-        </div>
+    <div class="content">
+        <h1 id="title">Connexion</h1>
+        <?php if(isset($error)): ?>
+            <div class="alert error">
+                <?= $error ?>
+                <?php var_dump($result); ?>
+            </div>
+        <?php endif; ?>
+        <form class="form" action="" method="POST">
+            <div class="field">
+                <label>Email</label>
+                <input name="email" type="email" required>
+            </div>
+            <div class="field">
+                <label>Mot de passe</label>
+                <input name="password" type="password" required>
+            </div>
+            <div class="submit">
+                <input type="submit" value="Se connecter">
+            </div>
+        </form>
     </div>
+</div>
